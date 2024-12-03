bc55455c79b92d6d76156bc0fe9e556b06951cfd
Sidorov13 <gunyaaa@gmail.com>
2024-12-02 20:21:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def handle_message(update: Update, context):
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': '192',
+                'preferredquality': '128',
             }],
             'ffmpeg_location': '/usr/bin/ffmpeg',
             'noplaylist': True,
